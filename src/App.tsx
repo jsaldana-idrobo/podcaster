733d273b7f04aefd3ff6da20f33045bfc856fdc4
Sebas <jsaldana.idrobo@gmail.com>
2023-07-21 16:30:40 +0200

    Only fetch podcats if more than a day has passed or it is the first time that we run the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Entry, Podcasts } from './types.d';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Entry, Podcasts } from "./types.d";
 
 function App() {
   const [podcasts, setPodcasts] = useState<Entry[]>([]);
 
   useEffect(() => {
-    fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
-      .then(async res => await res.json())
+    if (
+      !localStorage.getItem("fetchTime") ||
+      Math.round(new Date().getTime() / 1000) -
+        Number(localStorage.getItem("fetchTime")) >
+        86400
+    ) {
+      console.log("Entra");
+      getPodcasts();
+    } else {
+      setPodcasts(JSON.parse(localStorage.getItem("podcasts")!));
+    }
+  }, []);
+
+  const getPodcasts = () => {
+    fetch(
+      "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+    )
+      .then(async (res) => await res.json())
       .then((res: Podcasts) => {
-        setPodcasts(res.feed.entry)
-      }
-      ).catch(error => console.log(error)
-      );
-  }, [])
+        const pods = res.feed.entry;
+        setPodcasts(pods);
+        localStorage.setItem(
+          "fetchTime",
+          Math.round(new Date().getTime() / 1000).toString()
+        );
+        localStorage.setItem("podcasts", JSON.stringify(pods));
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Prueba tecnica - Podcaster</h1>
       {JSON.stringify(podcasts)}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
